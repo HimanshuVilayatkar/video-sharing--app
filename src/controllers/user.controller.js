bd39dcb389db3d8aafa4ffd15d9276487a6897f9
Himanshu vilayatkar <himanshuvilayatkar997@gmail.com>
2025-06-27 22:01:37 +0530

    add subscription model for user channel subscriptions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -197,4 +197,4 @@ const refreshAccessToken=asyncHandler(async(req,res)=>{
 
 
 })
-export { registerUser, loginUser, logoutUser ,refreshAccessToken}
+export { registerUser, loginUser, logoutUser ,refreshAccessToken} 
